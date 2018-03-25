b90abbf6bd9a80e48c82c75ecb4a424c7b8b7622
Bill Enright <bill@yisup.com>
2018-03-24 21:21:53 -0700

    reformatting, waitForA, bump version, publish

--- liftA-dynamodb.js
+++ liftA-dynamodb.js
@@ -23,112 +23,113 @@ SOFTWARE.
 */
 (function () {
 
-	'use strict';
+  'use strict';
 
-	function awsDynamo(dynamoConfig) {
-		let DynamoDB = require('aws-sdk/clients/dynamodb');
-		return new DynamoDB(dynamoConfig);
-	}
+  function awsDynamo(dynamoConfig) {
+    let DynamoDB = require('aws-sdk/clients/dynamodb');
+    return new DynamoDB(dynamoConfig);
+  }
 
-	module.exports = function (arw, dynamoConfig) {
+  module.exports = function (arw, dynamoConfig) {
 
-		return ((dynamo) => {
-			let cb = (x, cont, p, advance, err, data) => {
-				if (err) {
-					x = arw.Error(err, x);
-				} else {
-					x = data;
-				}
-				advance();
-				cont(x, p);
-			};
+    return ((dynamo) => {
+      let cb = (x, cont, p, advance, err, data) => {
+        if (err) {
+          x = arw.Error(err, x);
+        } else {
+          x = data;
+        }
+        advance();
+        cont(x, p);
+      };
 
-			function dynamoErrorBack(method) {
-				return function (x, cont, p) {
-					let cancelId;
-					let advance = () => p.advance(cancelId);
-					let req = dynamo[method](x, cb.bind(undefined, x, cont, p, advance));
-					cancelId = p.add(() => req.abort());
-					return cancelId;
-				};
-			}
+      function dynamoErrorBack(method) {
+        return function (x, cont, p) {
+          let cancelId;
+          let advance = () => p.advance(cancelId);
+          let req = dynamo[method](x, cb.bind(undefined, x, cont, p, advance));
+          cancelId = p.add(() => req.abort());
+          return cancelId;
+        };
+      }
 
-			function waitFor(state, params) {
-				return function (x, cont, p) {
-					let cancelId;
-					let advance = () => p.advance(cancelId);
-					let req = dynamo.waitFor(state(x), params(x), cb.bind(undefined, x, cont, p, advance));
-					cancelId = p.add(() => req.abort());
-					return cancelId;
-				};
-			}
+      let waitForA = (x, cont, p) => {
+        let cancelId;
+        let advance = () => p.advance(cancelId);
+        let req = dynamo.waitFor(x.state, x.params, cb.bind(undefined, x, cont, p, advance));
+        cancelId = p.add(() => req.abort());
+        return cancelId;
+      };
 
-			function documentClient(options) {
-				let aws = require('aws-sdk');
-				if (options) {
-					if (!options.service) {
-						options.service = dynamo;
-					}
-				} else {
-					options = { service: dynamo };
-				}
-				let doc = new aws.DynamoDB.DocumentClient(options);
-				function docErrorBack(method) {
-					return function (x, cont, p) {
-						let cancelId;
-						let advance = () => p.advance(cancelId);
-						let req = doc[method](x, cb.bind(undefined, x, cont, p, advance));
-						cancelId = p.add(() => req.abort());
-						return cancelId;
-					};
-				}
-				return {
-								batchGetA: docErrorBack('batchGet'),
-								batchWriteA: docErrorBack('batchWrite'),
-								createSet: (list, options) => doc.createSet(list, options),
-								deleteA: docErrorBack('delete'),
-								getA: docErrorBack('get'),
-								putA: docErrorBack('put'),
-								queryA: docErrorBack('query'),
-								scanA: docErrorBack('scan'),
-								updateA: docErrorBack('update')
-				};
-			}
+      function documentClient(options) {
+        let aws = require('aws-sdk');
+        if (options) {
+          if (!options.service) {
+            options.service = dynamo;
+          }
+        } else {
+          options = {
+            service: dynamo
+          };
+        }
+        let doc = new aws.DynamoDB.DocumentClient(options);
 
-			return {
-				documentClient: documentClient,
-				batchGetItemA: dynamoErrorBack('batchGetItem'),
-				batchWriteItemA: dynamoErrorBack('batchWriteItem'),
-				createBackupA: dynamoErrorBack('createBackup'),
-				createGlobalTableA: dynamoErrorBack('createGlobalTable'),
-				createTableA: dynamoErrorBack('createTable'),
-				deleteBackupA: dynamoErrorBack('deleteBackup'),
-				deleteItemA: dynamoErrorBack('deleteItem'),
-				deleteTableA: dynamoErrorBack('deleteTable'),
-				describeBackupA: dynamoErrorBack('describeBackup'),
-				describeContinuousBackupsA: dynamoErrorBack('describeContinuousBackups'),
-				describeGlobalTableA: dynamoErrorBack('describeGlobalTable'),
-				describeLimitsA: dynamoErrorBack('describeLimits'),
-				describeTableA: dynamoErrorBack('describeTable'),
-				describeTimeToLiveA: dynamoErrorBack('describeTimeToLive'),
-				getItemA: dynamoErrorBack('getItem'),
-				listBackupsA: dynamoErrorBack('listBackups'),
-				listGlobalTablesA: dynamoErrorBack('listGlobalTables'),
-				listTablesA: dynamoErrorBack('listTables'),
-				listTagsOfResourceA: dynamoErrorBack('listTagsOfResource'),
-				putItemA: dynamoErrorBack('putItem'),
-				queryA: dynamoErrorBack('query'),
-				restoreTableFromBackupA: dynamoErrorBack('restoreTableFromBackup'),
-				scanA: dynamoErrorBack('scan'),
-				tagResourceA: dynamoErrorBack('tagResource'),
-				untagResourceA: dynamoErrorBack('untagResource'),
-				updateGlobalTableA: dynamoErrorBack('updateGlobalTable'),
-				updateItemA: dynamoErrorBack('updateItem'),
-				updateTableA: dynamoErrorBack('updateTable'),
-				updateTimeToLiveA: dynamoErrorBack('updateTimeToLive'),
-				waitFor: waitFor
-				};
-		})(awsDynamo(dynamoConfig));
-	};
+        function docErrorBack(method) {
+          return function (x, cont, p) {
+            let cancelId;
+            let advance = () => p.advance(cancelId);
+            let req = doc[method](x, cb.bind(undefined, x, cont, p, advance));
+            cancelId = p.add(() => req.abort());
+            return cancelId;
+          };
+        }
+        return {
+          batchGetA: docErrorBack('batchGet'),
+          batchWriteA: docErrorBack('batchWrite'),
+          createSet: (list, options) => doc.createSet(list, options),
+          deleteA: docErrorBack('delete'),
+          getA: docErrorBack('get'),
+          putA: docErrorBack('put'),
+          queryA: docErrorBack('query'),
+          scanA: docErrorBack('scan'),
+          updateA: docErrorBack('update')
+        };
+      }
+
+      return {
+        documentClient: documentClient,
+        batchGetItemA: dynamoErrorBack('batchGetItem'),
+        batchWriteItemA: dynamoErrorBack('batchWriteItem'),
+        createBackupA: dynamoErrorBack('createBackup'),
+        createGlobalTableA: dynamoErrorBack('createGlobalTable'),
+        createTableA: dynamoErrorBack('createTable'),
+        deleteBackupA: dynamoErrorBack('deleteBackup'),
+        deleteItemA: dynamoErrorBack('deleteItem'),
+        deleteTableA: dynamoErrorBack('deleteTable'),
+        describeBackupA: dynamoErrorBack('describeBackup'),
+        describeContinuousBackupsA: dynamoErrorBack('describeContinuousBackups'),
+        describeGlobalTableA: dynamoErrorBack('describeGlobalTable'),
+        describeLimitsA: dynamoErrorBack('describeLimits'),
+        describeTableA: dynamoErrorBack('describeTable'),
+        describeTimeToLiveA: dynamoErrorBack('describeTimeToLive'),
+        getItemA: dynamoErrorBack('getItem'),
+        listBackupsA: dynamoErrorBack('listBackups'),
+        listGlobalTablesA: dynamoErrorBack('listGlobalTables'),
+        listTablesA: dynamoErrorBack('listTables'),
+        listTagsOfResourceA: dynamoErrorBack('listTagsOfResource'),
+        putItemA: dynamoErrorBack('putItem'),
+        queryA: dynamoErrorBack('query'),
+        restoreTableFromBackupA: dynamoErrorBack('restoreTableFromBackup'),
+        scanA: dynamoErrorBack('scan'),
+        tagResourceA: dynamoErrorBack('tagResource'),
+        untagResourceA: dynamoErrorBack('untagResource'),
+        updateGlobalTableA: dynamoErrorBack('updateGlobalTable'),
+        updateItemA: dynamoErrorBack('updateItem'),
+        updateTableA: dynamoErrorBack('updateTable'),
+        updateTimeToLiveA: dynamoErrorBack('updateTimeToLive'),
+        waitForA: waitForA
+      };
+    })(awsDynamo(dynamoConfig));
+  };
 
 })();
